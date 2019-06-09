029a374ffeb37d7cf1469de117a0af35afcf626a
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-06-09 16:44:26 -0400

    Creates a new 'components' folder, then creates a nested 'layout' folder, and also creates a 'Navbar.js' file within the layout folder

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Fragment, Component } from 'react';
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
 class App extends Component {
   render() {
-    const name = 'John Doe';
-    const loading = false;
-    const showName = true;
     return (
       <Fragment>
-        {loading ? <h4>Loading...</h4> : <h1>Hello {showName && name}</h1>}
+        <Navbar />
       </Fragment>
     );
   }
