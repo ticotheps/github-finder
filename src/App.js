5228d3b288c164743d444f3e0d8b35cd3b415ef9
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-06-09 16:07:28 -0400

    Replaces <div className='App'> tag with <React.Fragment> tag to 'hide' the parent <div className='App'> from showing up in the DOM, allowing only <div id='root'> to appear as if it is the only wrapping element around <h1> and <h2>

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className='App'>
+      <React.Fragment>
         <h1>Hello from React</h1>
-      </div>
+        <h2>Goodbye</h2>
+      </React.Fragment>
     );
   }
 }
