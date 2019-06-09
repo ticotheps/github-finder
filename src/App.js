dafe8513fcc6b9ff2365bc909cb9955548377eb7
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-06-09 16:33:01 -0400

    Creates an example of how to use '&&' as a way to write a conditional, that replaces the ternary statement

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ class App extends Component {
   render() {
     const name = 'John Doe';
     const loading = false;
+    const showName = true;
     return (
       <Fragment>
-        {loading ? <h4>Loading...</h4> : <h1>Hello {name}</h1>}
+        {loading ? <h4>Loading...</h4> : <h1>Hello {showName && name}</h1>}
       </Fragment>
     );
   }
