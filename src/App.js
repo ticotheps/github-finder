058f5b90812775bfdc7c4969076579e600261742
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-06-09 16:13:06 -0400

    Reverts back to JSX example (from vanilla JS example)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
+import React, { Fragment, Component } from 'react';
 import './App.css';
 
 class App extends Component {
   render() {
-    return React.createElement(
-      'div',
-      { className: 'App' },
-      React.createElement('h1', null, 'Hello from React')
+    return (
+      <Fragment>
+        <h1>Hello from React</h1>
+      </Fragment>
     );
   }
 }
