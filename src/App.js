718e872a487fc15aa58f2e138c16255bcf04ad29
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-06-09 16:26:07 -0400

    Creates an example for a ternary statement, which acts like an 'if' statement for rendering conditional JSX (inside of the returned parent <div>)

--- src/App.js
+++ src/App.js
@@ -5,14 +5,9 @@ class App extends Component {
   render() {
     const name = 'John Doe';
     const loading = false;
-
-    if (loading) {
-      return <h4>Loading...</h4>;
-    }
-
     return (
       <Fragment>
-        <h1>Hello {name}</h1>
+        {loading ? <h4>Loading...</h4> : <h1>Hello {name}</h1>}
       </Fragment>
     );
   }
