0556b97a6e773d9669620cebe5497e280aff4ed0
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-06-09 16:46:27 -0400

    Replace <div> tags in Navbar.js with <nav> and adds 'navbar' and 'bg-primary' classNames to add styles

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 export class Navbar extends Component {
   render() {
     return (
-      <div>
+      <nav className='navbar bg-primary'>
         <h1>Navbar</h1>
-      </div>
+      </nav>
     );
   }
 }
