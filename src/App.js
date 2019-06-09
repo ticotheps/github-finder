722ba468db947c41ff10115d1816bdd92bc017fd
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-06-09 16:09:27 -0400

    Imports 'Fragment' object with 'React' to shorten returned parent <div> to <Fragment>

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
+import React, { Fragment, Component } from 'react';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
+      <Fragment>
         <h1>Hello from React</h1>
         <h2>Goodbye</h2>
-      </React.Fragment>
+      </Fragment>
     );
   }
 }
