16b090165b2a7cb236f012a1805260a91a48b9e3
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-06-09 16:14:36 -0400

    Demonstrates example of how to add javascript into JSX

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const name = 'John Doe';
+
     return (
       <Fragment>
-        <h1>Hello from React</h1>
+        <h1>Hello {name.toUpperCase()}</h1>
       </Fragment>
     );
   }
