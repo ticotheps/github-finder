2f175031c4c35c57eb878b93076a1f035e9ef634
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-06-10 10:24:06 -0400

    Demonstrates how the constructor() function is NOT required; shows how 'state' object can be defined alone, without constructor()

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
 
 export class UserItem extends Component {
-  constructor() {
-    super();
-    this.state = {
-      id: 'id',
-      login: 'mojumbo',
-      avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-      html_url: 'https://github.com/mojombo',
-    };
-  }
+  state = {
+    id: 'id',
+    login: 'mojumbo',
+    avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+    html_url: 'https://github.com/mojombo',
+  };
 
   render() {
     return (
