541abbf63136cb83754152a5c03a1b5a323569b3
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-06-10 10:40:40 -0400

    Creates a new <Users /> to generate a list of users

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Fragment, Component } from 'react';
 import Navbar from './components/layout/Navbar';
-import UserItem from './components/users/UserItem';
+import Users from './components/users/Users';
 import './App.css';
 
 class App extends Component {
@@ -8,7 +8,7 @@ class App extends Component {
     return (
       <Fragment>
         <Navbar />
-        <UserItem />
+        <Users />
       </Fragment>
     );
   }
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,13 +1,6 @@
 import React, { Component } from 'react';
 
-export class UserItem extends Component {
-  state = {
-    id: 'id',
-    login: 'mojumbo',
-    avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-    html_url: 'https://github.com/mojombo',
-  };
-
+class UserItem extends Component {
   render() {
     const { login, avatar_url, html_url } = this.state;
 
