f2021004c9632c37482c5c4f661f742e69dd6aec
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-06-10 10:22:28 -0400

    Creates a constructor with a 'state' object nested inside; write hard-coded information from GitHub API for values of state; interpolates state values into the returned JSX

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,8 +1,33 @@
 import React, { Component } from 'react';
 
 export class UserItem extends Component {
+  constructor() {
+    super();
+    this.state = {
+      id: 'id',
+      login: 'mojumbo',
+      avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+      html_url: 'https://github.com/mojombo',
+    };
+  }
+
   render() {
-    return <div>User Item</div>;
+    return (
+      <div className='card text-center'>
+        <img
+          src={this.state.avatar_url}
+          alt='GitHub avatar for the user'
+          className='round-img'
+          style={{ width: '60px' }}
+        />
+        <h3>{this.state.login}</h3>
+        <div>
+          <a href={this.state.html_url} className='btn btn-dark btn-sm my-1'>
+            More
+          </a>
+        </div>
+      </div>
+    );
   }
 }
 
