eaff048e220fc66051a0a4d6e36a030716e2c378
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-06-10 10:03:02 -0400

    Demonstrates an example of how to use 'static defaultProps = {}', which is placed in the class, above the render() to provide default prop values, in the event that nothing is passed from a parent component

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 
 export class Navbar extends Component {
+  static defaultProps = {
+    title: 'GitHub Finder',
+    icon: 'fab fa-github',
+  };
+
   render() {
     return (
       <nav className='navbar bg-primary'>
