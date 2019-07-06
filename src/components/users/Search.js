341c2139c72f66772d7d428b945fc0be0424f5b5
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-07-05 22:50:00 -0400

    Removes unnecessary curly braces from 'onChange()'

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -6,10 +6,7 @@ export class Search extends Component {
     text: '',
   };
 
-  onChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
-
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
   render() {
     return (
       <div>
