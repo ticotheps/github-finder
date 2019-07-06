ba6ff0a204b30a50972e4f6606ed289c40e237bb
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-07-05 23:19:10 -0400

    Uncomments out CDM() function to display your follows on GitHub initially, before search begins

--- src/App.js
+++ src/App.js
@@ -11,15 +11,15 @@ class App extends Component {
     loading: false,
   };
 
-  // async componentDidMount() {
-  //   this.setState({ loading: true });
+  async componentDidMount() {
+    this.setState({ loading: true });
 
-  //   const res = await axios.get(`https://api.github.com/users?client_id=$
-  //     {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
-  //     {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    const res = await axios.get(`https://api.github.com/users/ticotheps/following?client_id=$
+      {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
+      {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
 
-  //   this.setState({ users: res.data, loading: false });
-  // }
+    this.setState({ users: res.data, loading: false });
+  }
 
   // Search GitHub users
   searchUsers = async text => {
