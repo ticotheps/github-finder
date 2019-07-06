d9b78e4dcb4cb9b2363237388f075971ddf3d31a
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-07-05 22:40:10 -0400

    Updates styles of navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,8 +4,9 @@ import PropTypes from 'prop-types';
 const Navbar = ({ icon, title }) => {
   return (
     <nav className='navbar bg-primary'>
-      <h1>
-        <i className={icon} /> {title}
+      <h1 className='title-navbar'>
+        <i className={icon} style={{ fontSize: '2.5rem', margin: '10px' }} />{' '}
+        {title}
       </h1>
     </nav>
   );
